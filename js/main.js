e7e0bc5258b070704f3841a98da8e6baf48c667e
Ekaterina <miteneva.kate@gmail.com>
2021-10-04 23:06:01 +0300

    started recording objects

--- js/main.js
+++ js/main.js
@@ -24,3 +24,65 @@ function getRdmFloatNumber(number1, number2, floatPoint) {
 }
 
 getRdmFloatNumber(50, 100, 3);
+
+//массив из фиксированного времени
+const TIME = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const FEATURES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+
+const author = {
+  avatar: '', //адрес изображения
+};
+
+const getInformation = () => {
+  //диапазон задала сама, так как в задании ничего про него нет. просто случайное целое число
+  //использую для price, guests rooms, но предполагаю, что позже будет это как то по другому
+  const randomNumber = getRandomNumber(0, 100);
+
+  const latitude = getRdmFloatNumber(35.65000, 35.70000);
+  const longitude = getRdmFloatNumber(139.70000, 139.80000);
+
+  // for (let i=0; i < 6; i++) {
+
+  // };
+
+  const location = {
+    lat: latitude,
+    lng: longitude,
+  };
+
+  const offer = {
+    title: 'Главное предложение месяца',
+    address: location.lat, //не совсем понимаю, почему линт ругается
+    price: randomNumber,
+    guests: randomNumber,
+    rooms: randomNumber,
+    type: TYPE[getRandomNumber(0, TYPE.length-1)],
+    checkin: TIME[getRandomNumber(0, TIME.length-1)],
+    checkout: TIME[getRandomNumber(0, TIME.length-1)],
+  };
+
+  return console.log(Object.values(offer).join(', '));
+};
+
+
+getInformation();
