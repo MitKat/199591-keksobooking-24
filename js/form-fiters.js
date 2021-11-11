189a8d7691100c84dfeda3176dd7fd1ad6c73ad3
Ekaterina <miteneva.kate@gmail.com>
2021-11-11 16:05:00 +0300

    victory over filtration

--- js/form-fiters.js
+++ js/form-fiters.js
@@ -1,30 +1,95 @@
-// import {getPopupItem} from './popup.js';
-// const Filt = {
-//   type: '',
-// };
+import {SIMILAR_OFFER_COUNT} from './utils/data.js';
+import {getBalun} from './map.js';
 
-// const filtersContainer = document.querySelector('.map__filters');
+const filtersContainer = document.querySelector('.map__filters');
+const filtersType = filtersContainer.querySelector('#housing-type');
+const filtersPrice = filtersContainer.querySelector('#housing-price');
+const filtersRooms = filtersContainer.querySelector('#housing-rooms');
+const filtersGuests = filtersContainer.querySelector('#housing-guests');
+const filtersFeatures = filtersContainer.querySelectorAll('.map__checkbox');
 
+const selectedStart = 'any';
 
-// const filtersType = filtersContainer.querySelector('#housing-type');
+const getFilterOffer = (offer) => {
 
-// const funcType = (offers) => {
-//   let rank = 0;
-//   filtersType.addEventListener('change', (evt) => {
-//     Filt.type = evt.target.value;
-//   });
+  // const selectedType = filtersType.value;
 
-//   if (offers.type === Filt.type) {
-//     rank += 1;
-//     console.log(Filt.type);
-//   }
-//   return rank;
-// }
+  if (filtersType.value !== offer.offer.type && filtersType.value !== selectedStart) {
+    return false;
+  }
 
-// const compareWizards = (wizardA, wizardB) => {
-//   const rankA = getWizardRank(wizardA);
-//   const rankB = getWizardRank(wizardB);
+  let rangePrice = '';
+  // const price  = offer.offer.price;
 
-//   return rankB - rankA;
-// }
+  if (offer.offer.price <= 10000) {
+    rangePrice = 'low';
+  } else if (offer.offer.price > 10000 && offer.offer.price < 50000) {
+    rangePrice = 'middle';
+  } else {
+    rangePrice = 'high';
+  }
+
+  // const selectedPrice = filtersPrice.value;
+
+  if (filtersPrice.value !== rangePrice && filtersPrice.value !== selectedStart) {
+    return false;
+  }
+
+  // const selectedRooms = parseInt(filtersRooms.value, 10);
+
+  if (parseInt(filtersRooms.value, 10) !== offer.offer.rooms && filtersRooms.value !== selectedStart) {
+    return false;
+  }
+
+  // const selectedGuests = parseInt(filtersGuests.value, 10);
+
+  if (parseInt(filtersGuests.value, 10) !== offer.offer.guests && filtersGuests.value !== selectedStart) {
+    return false;
+  }
+
+  return true;
+};
+
+const sortFeatures = (offer) => {
+  let rank = 0;
+
+  for (let i=0; i<filtersFeatures.length; i++) {
+    if (filtersFeatures[i].checked) {
+      if (offer.offer.features) {
+        for (let j=0; j < offer.offer.features.length; j++) {
+          if(offer.offer.features[j] === filtersFeatures[i].value) {
+            rank += 1;
+          }
+        }
+      } else {
+        rank -= 1;
+      }
+    }
+  }
+  return rank;
+};
+
+const compareOffers = (offerA, offerB) => {
+  const rankA = sortFeatures(offerA);
+  const rankB = sortFeatures(offerB);
+  return rankB - rankA;
+};
+
+const setFilterOffers = (callback) => {
+  filtersContainer.addEventListener('change', () => {
+
+    callback();
+  });
+};
+
+const renderOffer = (offers) => {
+  const copyOffers = offers
+    .slice()
+    .filter(getFilterOffer)
+    .sort(compareOffers)
+    .slice(0, SIMILAR_OFFER_COUNT);
+  getBalun(copyOffers);
+};
+
+export {setFilterOffers, renderOffer};
 
--- js/main.js
+++ js/main.js
@@ -1,113 +1,21 @@
-
 import './popup.js';
 import './form-fiters.js';
 import {getData} from './utils/api.js';
 import {showSuccessSent} from './message-sent-form.js';
-import {showAlert} from './utils/data.js';
+import {showAlert, SIMILAR_OFFER_COUNT} from './utils/data.js';
 import {setFormSubmit, setOnFormReset} from './form-offer.js';
 import {getBalun, resetMainMarker, resetMarker} from './map.js';
-
-const filterElement = {
-  rooms: 0,
-  type: '',
-  price: '',
-};
-
-const filtersContainer = document.querySelector('.map__filters');
-
-const filtersType = filtersContainer.querySelector('#housing-type');
-const filtersPrice = filtersContainer.querySelector('#housing-price');
-const filtersRooms = filtersContainer.querySelector('#housing-rooms');
-
-const setTypeChange = (callback) => {
-  filtersType.addEventListener('change', (evt) => {
-    filterElement.type = evt.target.value;
-    callback();
-  });
-};
-
-const setPriceChange = (callback) => {
-  filtersPrice.addEventListener('change', (evt) => {
-    filterElement.price = evt.target.value;
-    callback();
-  });
-};
-
-const setRoomsChange = (callback) => {
-  filtersRooms.addEventListener('change', (evt) => {
-    filterElement.rooms = Number(evt.target.value);
-    callback();
-  });
-};
-
-const funcType = (offer) => {
-  let rank = 0;
-
-  if (offer.offer.type === filterElement.type) {
-    rank += 3;
-  }
-
-  let rangePrice = '';
-  const price  = Number(offer.offer.price);
-
-  if (price <= 10000) {
-    rangePrice = 'low';
-  } else if (price > 10000 && price < 50000) {
-    rangePrice = 'middle';
-  } else {
-    rangePrice = 'high';
-  }
-
-  if (rangePrice === filterElement.price) {
-    rank += 2;
-  }
-
-  if (offer.offer.rooms === filterElement.rooms) {
-    rank += 1;
-  }
-
-  return rank;
-};
-
-const compareOffers = (offerA, offerB) => {
-  const rankA = funcType(offerA);
-  const rankB = funcType(offerB);
-  return rankB - rankA;
-};
-
-const SIMILAR_OFFER_COUNT = 10;
+import {setFilterOffers, renderOffer} from './form-fiters.js';
+import {debounce} from './utils/debounce.js';
 
 getData(
   (offers) => {
     getBalun(offers.slice(0, SIMILAR_OFFER_COUNT));
-    setTypeChange(() => {
-      resetMarker();
-      getBalun(
-        offers
-          .slice()
-          .sort(compareOffers)
-          .slice(0, SIMILAR_OFFER_COUNT),
-      );
-    });
-    setPriceChange(() => {
-      resetMarker();
-      getBalun(
-        offers
-          .slice()
-          .sort(compareOffers)
-          .slice(0, SIMILAR_OFFER_COUNT),
-      );
-    });
-    setRoomsChange(() => {
-      resetMarker();
-      getBalun(
-        offers
-          .slice()
-          .sort(compareOffers)
-          .slice(0, SIMILAR_OFFER_COUNT),
-      );
-    });
-
+    setFilterOffers(debounce(
+      () => {
+        resetMarker();
+        renderOffer(offers);
+      }));
   },
   () => showAlert('Ошибка на сервере. Подождите, мы уже решаем проблему.'),
 );
