3f1ed2d31b76abc4e30c06d9f419fbb232d1645f
Ekaterina <miteneva.kate@gmail.com>
2021-11-12 11:01:59 +0300

    filter features

--- js/form-fiters.js
+++ js/form-fiters.js
@@ -1,6 +1,10 @@
 import {SIMILAR_OFFER_COUNT} from './utils/data.js';
 import {getBalun} from './map.js';
 
+const PriceRange = {
+  low: 10000,
+  high: 50000,
+};
 const filtersContainer = document.querySelector('.map__filters');
 const filtersType = filtersContainer.querySelector('#housing-type');
 const filtersPrice = filtersContainer.querySelector('#housing-price');
@@ -8,76 +12,65 @@ const filtersRooms = filtersContainer.querySelector('#housing-rooms');
 const filtersGuests = filtersContainer.querySelector('#housing-guests');
 const filtersFeatures = filtersContainer.querySelectorAll('.map__checkbox');
 
-const selectedStart = 'any';
+const ANY_VALUE = 'any';
 
 const getFilterOffer = (offer) => {
 
-  // const selectedType = filtersType.value;
-
-  if (filtersType.value !== offer.offer.type && filtersType.value !== selectedStart) {
+  if (filtersType.value !== offer.offer.type && filtersType.value !== ANY_VALUE) {
     return false;
   }
 
   let rangePrice = '';
-  // const price  = offer.offer.price;
 
-  if (offer.offer.price <= 10000) {
+  if (offer.offer.price <= PriceRange.low) {
     rangePrice = 'low';
-  } else if (offer.offer.price > 10000 && offer.offer.price < 50000) {
+  } else if (offer.offer.price > PriceRange.low && offer.offer.price < PriceRange.high) {
     rangePrice = 'middle';
   } else {
     rangePrice = 'high';
   }
 
-  // const selectedPrice = filtersPrice.value;
-
-  if (filtersPrice.value !== rangePrice && filtersPrice.value !== selectedStart) {
+  if (filtersPrice.value !== rangePrice && filtersPrice.value !== ANY_VALUE) {
     return false;
   }
 
-  // const selectedRooms = parseInt(filtersRooms.value, 10);
-
-  if (parseInt(filtersRooms.value, 10) !== offer.offer.rooms && filtersRooms.value !== selectedStart) {
+  const selectedRooms = parseInt(filtersRooms.value, 10);
+  if (selectedRooms !== offer.offer.rooms && filtersRooms.value !== ANY_VALUE) {
     return false;
   }
 
-  // const selectedGuests = parseInt(filtersGuests.value, 10);
-
-  if (parseInt(filtersGuests.value, 10) !== offer.offer.guests && filtersGuests.value !== selectedStart) {
+  const selectedGuests = parseInt(filtersGuests.value, 10);
+  if (selectedGuests !== offer.offer.guests && filtersGuests.value !== ANY_VALUE) {
     return false;
   }
-
-  return true;
-};
-
-const sortFeatures = (offer) => {
-  let rank = 0;
 
   for (let i=0; i<filtersFeatures.length; i++) {
     if (filtersFeatures[i].checked) {
       if (offer.offer.features) {
-        for (let j=0; j < offer.offer.features.length; j++) {
-          if(offer.offer.features[j] === filtersFeatures[i].value) {
-            rank += 1;
-          }
+        const checkedElement = filtersFeatures[i].value;
+        const featureElement = offer.offer.features.find((valueFeature) => valueFeature.includes(checkedElement));
+
+        if(checkedElement !== featureElement) {
+          return false;
         }
       } else {
-        rank -= 1;
+        return false;
       }
     }
   }
-  return rank;
+  return true;
 };
 
 const compareOffers = (offerA, offerB) => {
-  const rankA = sortFeatures(offerA);
-  const rankB = sortFeatures(offerB);
-  return rankB - rankA;
+  if (offerA.offer.features && offerB.offer.features) {
+    const rankA = offerA.offer.features.length;
+    const rankB = offerB.offer.features.length;
+    return rankB - rankA;
+  }
 };
 
 const setFilterOffers = (callback) => {
   filtersContainer.addEventListener('change', () => {
-
     callback();
   });
 };
